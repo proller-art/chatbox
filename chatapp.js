50eedbf4568ba6530075530226145588d5b63bac
proller-art <105773133+proller-art@users.noreply.github.com>
2022-05-18 00:40:23 -0700

    Update chatapp.js

--- chatapp.js
+++ chatapp.js
@@ -4,11 +4,30 @@ const client = new tmi.Client({
 
 client.connect();
 
-var chatlog = {};
+//var chatlog = {};
 
 client.on('message', (channel, tags, message, self) => {
 	// "Alca: Hello, World!"
 	// console.log(`${tags['display-name']}: ${message}`);
-	chatlog = chatlog + `${tags['display-name']}: ${message}` + "\n";
-	document.getElementById("chatbox").innerText = chatlog;
+	//chatlog.push(formatEmotes(message, tags.emotes));
+	document.getElementById("chatbox").innerHTML= formatEmotes(message, tags.emotes);
 });
+
+function formatEmotes(text, emotes) {
+	var splitText = text.split('');
+	for(var i in emotes) {
+		var e = emotes[i];
+		for(var j in e) {
+			var mote = e[j];
+			if(typeof mote == 'string') {
+				mote = mote.split('-');
+				mote = [parseInt(mote[0]), parseInt(mote[1])];
+				var length =  mote[1] - mote[0],
+					empty = Array.apply(null, new Array(length + 1)).map(function() { return '' });
+				splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
+				splitText.splice(mote[0], 1, '<img class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v1/' + i + '/3.0">');
+			}
+		}
+	}
+	return splitText.join('');
+}
