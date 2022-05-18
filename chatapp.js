4aa790f84733ad4c2492c74c912186bdf2e213f2
proller-art <105773133+proller-art@users.noreply.github.com>
2022-05-18 03:00:50 -0700

    Update chatapp.js

--- chatapp.js
+++ chatapp.js
@@ -4,17 +4,37 @@ const client = new tmi.Client({
 
 client.connect();
 
-var chatlog = "";
+var chatStack = [];
 
 client.on('message', (channel, tags, message, self) => {
-	// "Alca: Hello, World!"
-	// console.log(`${tags['display-name']}: ${message}`);
-	chatlog = chatlog + `${tags['display-name']}: ${message}` + \n;
-	//document.getElementById("chatbox").innerText = formatEmotes(message, tags.emotes);
-	//document.getElementById("chatbox").innerText = "beepo";
+	//chatStack = chatStack + `${tags['display-name']}: ${message}` + "\n";
+	const chatMessage = {tags: tags, message: message};
+	//chatStack.push(chatMessage);
+	updateChat(chatStack, chatMessage);
+	printChat(chatStack, "chatbox");
 });
 
-/*function formatEmotes(text, emotes) {
+
+function updateChat(chat, message) {
+	chat.push(message);
+	if (chat.length > 5) {
+		chat.shift();
+	}
+}
+
+function printChat(chat, id) {
+	var result = "";
+	for (var i = 0; i < chat.length; i++) {
+		if (chat[i].tags.subscriber == true) {
+			result = result + '<div class="chatMessage"><div class="chatNameSub">' + `${chat[i].tags['display-name']}` + '</div>' + `${formatEmotes(chat[i].message, chat[i].tags.emotes)}` + "</div>";
+		} else {
+			result = result + '<div class="chatMessage"><div class="chatName">' + `${chat[i].tags['display-name']}` + '</div>' + `${formatEmotes(chat[i].message, chat[i].tags.emotes)}` + "</div>";
+		}
+	}
+	document.getElementById(id).innerHTML = result;
+}
+
+function formatEmotes(text, emotes) {
 	var splitText = text.split('');
 	for(var i in emotes) {
 		var e = emotes[i];
@@ -26,9 +46,9 @@ client.on('message', (channel, tags, message, self) => {
 				var length =  mote[1] - mote[0],
 					empty = Array.apply(null, new Array(length + 1)).map(function() { return '' });
 				splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
-				splitText.splice(mote[0], 1, '<img class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v1/' + i + '/3.0">');
+				splitText.splice(mote[0], 1, '<img class="emote" src="https://static-cdn.jtvnw.net/emoticons/v2/' + i + '/default/dark/1.0">');
 			}
 		}
 	}
 	return splitText.join('');
-}*/
+}
